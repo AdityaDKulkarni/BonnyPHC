15111d98623acd4a4e944f169113b82fa150acc7
AdityaDKulkarni <kulkarniaditya1997@gmail.com>
2019-08-13 11:47:19 +0530

    Changed base url

--- app/src/main/java/com/bonny/bonnyphc/config/URLConstants.java
+++ app/src/main/java/com/bonny/bonnyphc/config/URLConstants.java
@@ -6,6 +6,7 @@ package com.bonny.bonnyphc.config;
 
 public interface URLConstants {
 
-    static String BASE_URL = "https://ameydarekar.pythonanywhere.com/";
+    //static String BASE_URL = "https://ameydarekar.pythonanywhere.com/";
     //static String BASE_URL = "https://e88c9f4b.ngrok.io/";
+    static String BASE_URL = "https://ameydarekar.me/";
 }
